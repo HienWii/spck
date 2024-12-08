92e72ef7272b26bef2f19485fc7492cf1a4ec4da
HienWii <hiennguyen20020002@gmail.com>
2024-12-08 18:03:46 +0700

    dangnhap

---
+++ login/login.js
@@ -0,0 +1,17 @@
+const loginBtn = document.querySelector("#loginBtn");
+const emailLogin = document.querySelector("#emailLogIn");
+const passwordLogin = document.querySelector("#passwordLogIn")
+const userInfo = JSON.parse(localStorage.getItem("user"));
+const email = userInfo.email;
+const password = userInfo.password;
+loginBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    const emailValue = emailLogin.value;
+    const passwordValue = passwordLogin.value;
+    if (emailValue === email && passwordValue === password) {
+        alert("Đăng nhập hợp lệ");
+        window.location = "../home.html";
+    } else {
+        alert("Tài khoản và mật khẩu không hợp lệ");
+    }
+});
